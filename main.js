8d6321a4342ffac99f42fe0ad1d11abd17e46650
leonardo-o-s-s <leo.oliveira.silva227@gmail.com>
2023-01-23 17:30:01 -0300

    Dia 2 - Feito

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 /*
-Com essa estrutura base criada, você precisará vincular uma função ao evento de submissão do formulário. 
-A função vinculada deverá pegar o valor informado nos campos nome 
-e data de nascimento e imprimi-los no console. 
+Com essa estrutura base criada, você precisará vincular uma função ao evento de submissão do formulário.
+A função vinculada deverá pegar o valor informado nos campos nome
+e data de nascimento e imprimi-los no console.
 */
-const botao = document.getElementById("enviar")
-const valores = []
+// const botao = document.getElementById("enviar")
+// const valores = []
 
-botao.addEventListener("click", (evento) => {
-    const nome = document.querySelector("#nome").value
-    const dataNascimento = document.querySelector("#data-nascimento").value
-    evento.preventDefault()
-    console.log(nome)
-    console.log(dataNascimento)
-})
+// botao.addEventListener("click", (evento) => {
+//     const nome = document.querySelector("#nome").value
+//     const dataNascimento = document.querySelector("#data-nascimento").value
+//     evento.preventDefault()
+//     console.log(nome)
+//     console.log(dataNascimento)
+// })
 
 
 // function mensagem(pessoa, nascimento) {
